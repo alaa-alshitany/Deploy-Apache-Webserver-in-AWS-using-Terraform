204d0425d424ff910bd88f3bdf17deb0a8543e92
alaaWalid <alaa2alshitany@gmail.com>
2024-07-04 14:21:54 +0300

    Added Policy for session manager access

--- Terraform/IAM-Role.tf
+++ Terraform/IAM-Role.tf
@@ -38,3 +38,22 @@ resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
   role       = aws_iam_role.s3-role.name
   policy_arn = aws_iam_policy.s3_policy.arn
 }
+
+# session manager policy 
+resource "aws_iam_policy" "session_manager_policy" {
+  name        = "Session_Manager_Policy"
+  description = "Policy for Session Manager Access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Action    = [
+          "ssm:StartSession"
+        ],
+        Resource  = "*"
+      }
+    ]
+  })
+}
