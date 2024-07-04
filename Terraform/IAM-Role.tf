acff44e34c4ced913bfd7f845b57a766eda15beb
alaaWalid <alaa2alshitany@gmail.com>
2024-07-04 14:30:15 +0300

    Rename Role to be more descriptive

--- Terraform/IAM-Role.tf
+++ Terraform/IAM-Role.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "s3-role" {
-  name = "s3-Role"
+resource "aws_iam_role" "s3_and_session_manager_role" {
+  name = "s3_and_session_manager_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -35,7 +35,7 @@ resource "aws_iam_policy" "s3_policy" {
 
 # attach S3 policy to IAM role
 resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
-  role       = aws_iam_role.s3-role.name
+  role       = aws_iam_role.s3_and_session_manager_role.name
   policy_arn = aws_iam_policy.s3_policy.arn
 }
 
