dbdb858d23fb5b5b8a9b956118f08e8b191fea26
alaaWalid <alaa2alshitany@gmail.com>
2024-07-04 12:08:48 +0300

    Added another bucket to store the webserver configuration

--- Terraform/s3-buckets.tf
+++ Terraform/s3-buckets.tf
@@ -4,3 +4,10 @@ resource "aws_s3_bucket" "bk" {
     Name        = var.bucket_names[0]
   }
 }
+
+resource "aws_s3_bucket" "bk_web" {
+  bucket = var.bucket_names[1]
+  tags = {
+    Name        = var.bucket_names[1]
+  }
+}
