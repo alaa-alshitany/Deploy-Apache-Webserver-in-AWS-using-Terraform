e0792a4e10a19c78611124179b315611e3d77c7c
alaaWalid <alaa2alshitany@gmail.com>
2024-07-04 12:09:38 +0300

    Create s3 object to store the user data script

--- Terraform/s3-buckets.tf
+++ Terraform/s3-buckets.tf
@@ -11,3 +11,10 @@ resource "aws_s3_bucket" "bk_web" {
     Name        = var.bucket_names[1]
   }
 }
+
+resource "aws_s3_object" "user-data-obj" {
+  bucket = aws_s3_bucket.bk_web.bucket
+  key = "user-data.sh"
+  source = "../Scripts/user-data.sh"
+  acl = "private"
+}
