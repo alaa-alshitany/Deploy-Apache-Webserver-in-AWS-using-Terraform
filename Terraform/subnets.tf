489dc8127e08f468ed869c398f71a02e99519bcf
alaaWalid <alaa2alshitany@gmail.com>
2024-07-07 21:28:48 +0300

    update public subnet resource

--- Terraform/subnets.tf
+++ Terraform/subnets.tf
@@ -1,8 +1,9 @@
 resource "aws_subnet" "public_subnet" {
-  count             = length(var.public_subnet_cidr)
+  count      = length(var.public_subnet_cidr)
   vpc_id            = aws_vpc.web_vpc.id
   cidr_block        = element(var.public_subnet_cidr, count.index)
-  availability_zone = var.azs[2] 
+  availability_zone       = element(var.azs, count.index)
+  map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet ${count.index + 1}"
   }
