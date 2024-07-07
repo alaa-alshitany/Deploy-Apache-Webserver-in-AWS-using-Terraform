6ee90e284f5106dab97b2b0722738bc5f7006236
alaaWalid <alaa2alshitany@gmail.com>
2024-07-07 21:47:46 +0300

    Added Route tables for public and private subnets

---
+++ Terraform/Route-Table.tf
@@ -0,0 +1,28 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.web_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  count     = length(aws_subnet.public_subnet)
+  subnet_id = element(aws_subnet.public_subnet[*].id, count.index)
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.web_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+  }
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+  count          = length(aws_subnet.private_subnets)
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  route_table_id = aws_route_table.private_rt.id
+}
