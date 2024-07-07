ed2fdb77a6910c465624b81be709c6a5a663fd88
alaaWalid <alaa2alshitany@gmail.com>
2024-07-07 21:27:54 +0300

    Update CIDR of public and private subnets

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -22,11 +22,11 @@ variable "azs" {
 variable "public_subnet_cidr" {
  type        = list(string)
  description = "Public Subnet CIDR"
- default     = ["10.0.1.0/24"]
+ default     = ["10.0.1.0/24","10.0.2.0/24" ]
 }
  
 variable "private_subnet_cidrs" {
  type        = list(string)
  description = "Private Subnet CIDR values"
- default     = ["10.0.2.0/24", "10.0.3.0/24"]
+ default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
