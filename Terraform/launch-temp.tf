314c50f4b2adb5cdb2084e42dcfcc1b6f90ae2e4
alaaWalid <alaa2alshitany@gmail.com>
2024-07-07 21:43:29 +0300

    updated launch template

--- Terraform/launch-temp.tf
+++ Terraform/launch-temp.tf
@@ -1,12 +1,13 @@
 resource "aws_iam_instance_profile" "instance_profile" {
   name = "web_server_profile"
-  role = aws_iam_role.s3_and_session_manager_role.name
+  role = aws_iam_role.Terraform_Role.name
 }
 
 resource "aws_launch_template" "launch_temp" {
   name          = "web_config"
-  image_id      = "ami-07f406bc85a990c00"
+  image_id      = aws_ami_from_instance.golden_ami.id
   instance_type = "t2.micro"
+  key_name = "key"
   user_data = base64encode(<<-EOF
               #!/bin/bash
               aws s3 cp s3://web-conf-bk/user-data.sh /home/ec2-user/user-data.sh
@@ -17,4 +18,5 @@ resource "aws_launch_template" "launch_temp" {
   iam_instance_profile {
     name = aws_iam_instance_profile.instance_profile.name 
   } 
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
 }
